aee68653b03c88e0e4583400b5df53184456a470
GuanMiAoo <46982648+GuanMiAoo@users.noreply.github.com>
2019-03-23 21:39:21 +0800

    Add files via upload

---
+++ hw1/103080010_林冠妙_hw1.cpp
@@ -0,0 +1,22 @@
+/* date: 0221
+purpose: 名片列印程式碼
+姓名:林冠妙 103080010
+*/
+#include <iostream>
+using namespace std;
+
+// main function
+int main()
+{
+	// design a personal card
+
+	cout << "\* \* \* \* \* \* \* \* \* \* \*\n\n" 
+	<<"\*   Guan-Miao Lin   \*\n"
+	<<"\*     103080010     \*\n" 
+	<<"\*     NTHU LSIP     \*\n"
+	<<"\*   Email: xxxxxx   \*\n\n"
+	<< "\* \* \* \* \* \* \* \* \* \* \*\n";
+	
+	return 0;
+	
+}
